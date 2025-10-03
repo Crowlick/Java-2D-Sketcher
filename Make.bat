44a881285e8377d12032d7c101280f7176be3d0d
Crowlick <ivan-kochet0v@mailru>
2025-10-03 22:04:38 +0300

    New bat files

--- Make.bat
+++ Make.bat
@@ -1,5 +1,8 @@
+@cd/d "%~dp0"
 if not exist bin mkdir bin
 
-./jdk1.1.8/bin/javac.exe -d bin -cp "./lib/jars/lwjgl.jar:./lib/jars/lwjgl_util.jar:./lib/jars/slick-util.jar:./lib/jars/json-java.jar"  src/Contraints/*.java  ./src/engineTester/*.java ./src/entities/*.java src/main/*.java src/renderEngine/*.java src/shaders/*.java src/toolBox/*.java
+"./jdk-17.0.8/bin/javac.exe" -d bin -cp "./lib/jars/lwjgl.jar;./lib/jars/lwjgl_util.jar;./lib/jars/slick-util.jar;./lib/jars/json-java.jar"  src/Contraints/*.java  ./src/engineTester/*.java ./src/entities/*.java src/main/*.java src/renderEngine/*.java src/shaders/*.java src/toolBox/*.java
 
-copy src/shaders/*.txt bin/shaders/
+copy src\shaders\*.txt bin\shaders\
+
+pause
--- Run.bat
+++ Run.bat
@@ -1,1 +1,3 @@
-./jdk1.1.8/bin/java.exe "-Djava.library.path=./lib/natives/" -cp "./lib/jars/lwjgl.jar:./lib/jars/lwjgl_util.jar:./lib/jars/slick-util.jar:./lib/jars/json-java.jar" main.Main
+@cd/d "%~dp0"
+"./jdk-17.0.8/bin/java.exe" "-Djava.library.path=./lib/natives/" -cp "./lib/jars/lwjgl.jar;./lib/jars/lwjgl_util.jar;./lib/jars/slick-util.jar;./lib/jars/json-java.jar;./bin" main.Main
+pause
